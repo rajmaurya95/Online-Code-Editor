7c1aa95ec4c91c497cca67930d500cbe7d47669f
Raj <gautamshakya9528@gmail.com>
2022-05-29 04:03:05 +0530

    Updates

---
+++ precache-manifest.85c1f44efc2fa424071f4a00141c4e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c62f22789102b69c6843",
+    "url": "/Online-Code-Editor/static/css/main.58d0392f.chunk.css"
+  },
+  {
+    "revision": "c62f22789102b69c6843",
+    "url": "/Online-Code-Editor/static/js/main.c62f2278.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/Online-Code-Editor/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "69568aa8518e49e1c8ee",
+    "url": "/Online-Code-Editor/static/js/2.69568aa8.chunk.js"
+  },
+  {
+    "revision": "2c5c541395c8ca330fef",
+    "url": "/Online-Code-Editor/static/js/runtime~main.2c5c5413.js"
+  },
+  {
+    "revision": "5c7ac2af3d79a9fc1c88ccd0b8be2bd4",
+    "url": "/Online-Code-Editor/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Online-Code-Editor/precache-manifest.85c1f44efc2fa424071f4a00141c4e81.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Online-Code-Editor/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
